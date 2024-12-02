1abb6a075f21c54cdc83af2eba926ea3a63c2c7a
rraffaa <148355948+rraffaa@users.noreply.github.com>
2024-12-02 18:59:53 -0300

    Add Tree.java

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PlantingTrees
-This is a fun exercise of playing with shapes of trees using "*" and coding in Java. In this project, we are printing a tree shape to the screen by using simple `System.out.println` statements in Java.
+
+This is a fun exercise of playing with shapes of trees using "*" and coding in Java. In this project, we are printing a tree shape to the screen by using simple `System.out.println` statements in Java. 
+
+## Features
+
+- Introduction message to users
+- Project goal description
+- Tree printed using asterisks ("*")
---
+++ Tree.java
@@ -0,0 +1,31 @@
+class Tree {
+
+  public static void main(String[] args) {
+
+System.out.println("Hey there, I'm Rafa and I'm learning to code in Java!");
+System.out.println("I'm going to plant a tree today!");
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+    System.out.println("     ");   
+
+    System.out.println("    *  ");
+    System.out.println("   ***  ");
+    System.out.println("  ***** ");
+    System.out.println(" ******* ");
+    System.out.println("    *  ");
+    System.out.println("    *  ");   
+    System.out.println("      "); 
+
+    System.out.println("   ** **   ");
+    System.out.println("  *******  ");
+    System.out.println(" ********* ");
+    System.out.println("  ******* ");
+    System.out.println("     *  ");
+    System.out.println("     *  ");   
+
+  }
+}
